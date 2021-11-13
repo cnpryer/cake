354fbae3e67ca57286a4d662653cd2ed74bbe5a0
Chris <cnpryer@gmail.com>
2021-11-13 16:46:38 -0500

    added File trait

--- cake_lib/src/lib.rs
+++ cake_lib/src/lib.rs
@@ -1,12 +1,28 @@
+use std::fs;
+
+// The File
+trait File {
+    fn new(path: &String) -> FileObject;
+    fn is_valid(&self) -> bool;
+    fn validate(&self);
+    fn get_path(&self) -> &String;
+    fn get_name(&self) -> &String;
+    fn get_type(&self) -> &String;
+    fn get_size(&self) -> &u32;
+
+    // TODO: display lib
+    fn print_metadata(&self);
+    fn print_head(&self);
+}
+
 // File
 //
 // metadata (FileMetadata): Stored metedata from a file target
 // data (FileData): Stored data from a file target
-pub struct File {
+pub struct FileObject {
     metadata: FileMetadata,
-    data: FileData<String>
+    data: FileData<String>,
 }
-
 
 // FileMetadata
 //
@@ -18,58 +34,69 @@ struct FileMetadata {
     path: String,
     name: String,
     size: u32,
-    filetype: String
+    filetype: String,
 }
 
 // FileData
 //
 // data (Vec<String>): Loaded file data. TODO: move away from current data types.
-struct FileData<String> { 
-    data: Vec<String>
+struct FileData<String> {
+    data: Vec<String>,
 }
 
 // File
 //
 // Initialized File from `path` of target file.
-impl File {
-
+impl File for FileObject {
     // Create file from filepath
-    pub fn new(path: &String) -> File {
-        let metadata = init_file_metadata_from_filepath(&path);
-        let data = FileData { data: vec![] };
+    fn new(path: &String) -> FileObject {
+        let metadata = load_metadata(&path);
+        let data = load_data(&path);
 
-        File { metadata, data }
+        FileObject { metadata, data }
+    }
+
+    // return true if file is valid
+    fn is_valid(&self) -> bool {
+        true
+    }
+
+    // validate file
+    fn validate(&self) {
+        if !self.is_valid() {
+            panic!("File is invalid");
+        }
     }
 
     // get path to file
-    pub fn get_path(&self) -> &String {
-       &self.metadata.path
+    fn get_path(&self) -> &String {
+        &self.metadata.path
     }
 
     // get size of file
-    pub fn get_size(&self) -> &u32 {
-      &self.metadata.size
-     }
+    fn get_size(&self) -> &u32 {
+        &self.metadata.size
+    }
 
     // get name of file
-    pub fn get_name(&self) -> &String {
+    fn get_name(&self) -> &String {
         &self.metadata.name
     }
 
     // get file's type
-    pub fn get_type(&self) -> &String {
+    fn get_type(&self) -> &String {
         &self.metadata.filetype
     }
 
     // print file metadata such as path, size, type
-    pub fn print_metadata(self) {
+    fn print_metadata(&self) {
         println!("Filepath: {}", self.get_path());
         println!("Filename: {}", self.get_name());
         println!("Size: {}", self.get_size());
     }
 
     // print head of file data
-    pub fn print_head(self) {
+    fn print_head(&self) {
         println!("Head: {:?}", &self.data.data[0..5]);
     }
 }
@@ -91,7 +118,7 @@ fn get_filetype_from_path(path: &String) -> Option<String> {
 }
 
 // generates metadata from a target filepath
-fn init_file_metadata_from_filepath(path: &String) -> FileMetadata {
+fn load_metadata(path: &String) -> FileMetadata {
     let name = get_filename_from_path(&path);
 
     if name.is_none() {
@@ -103,10 +130,23 @@ fn init_file_metadata_from_filepath(path: &String) -> FileMetadata {
     if filetype.is_none() {
         panic!("Ivalid filetype");
     }
-    
-    FileMetadata {path: path.clone(), name: name.unwrap(), size: 0, filetype: filetype.unwrap()}
+
+    FileMetadata {
+        path: path.clone(),
+        name: name.unwrap(),
+        size: 0,
+        filetype: filetype.unwrap(),
+    }
 }
 
+// generate file data from a target filepath
+fn load_data(path: &String) -> FileData<String> {
+    let contents =
+        fs::read_to_string(path.as_str()).expect("Something went wrong reading the file");
+    let data = contents.split("/n").map(|s| s.to_string()).collect();
+
+    FileData { data }
+}
 
 #[cfg(test)]
 mod tests {
@@ -114,11 +154,12 @@ mod tests {
 
     #[test]
     fn test_fake_file() {
-        let path = "./test/file.csv".to_string();
-        let file = File::new(&path);
-        
-        assert_eq!(*file.get_name(), "file.csv");
+        let path = "../testing/test_data.csv".to_string();
+        let file = FileObject::new(&path);
+
+        assert_eq!(*file.get_name(), "test_data.csv");
         assert_eq!(*file.get_size(), 0);
         assert_eq!(*file.get_path(), path);
+        assert!(file.is_valid());
     }
 }
